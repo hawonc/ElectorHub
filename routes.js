de41710c369a881afeb831b71a43db712d45e60b
Joshua (Hawon) Cho <103012697+hawonc@users.noreply.github.com>
2024-12-07 13:13:04 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
     password: 'password',
     database: 'eligible_voters'
